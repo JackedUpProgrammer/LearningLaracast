03a7da5360c7ab5b8307fb25dda66c0f58ef8a0c
Johan Scheepers <johan@jumpingfoxsoftware.com>
2023-05-01 19:15:34 +0200

    first commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,43 @@
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+   
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Blog</title>
+
+    {{-- link css --}}
+    <link rel="stylesheet" href="/app.css">
+
+    {{-- link js --}}
+    <script src="/app.js"></script>
+
+ </head>
+ <body>
+  <article>
+   <h1>
+     <a href="/posts/my-first-post">My First Post</a> 
+   </h1>
+   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste minus ad reiciendis unde, consequuntur id harum eveniet sapiente dolor quam eligendi nesciunt eos est earum explicabo, aspernatur esse quis enim!</p>
+  </article>
+
+
+
+   <article>
+    <h1>
+      <a href="/posts/my-second-post">My Second Post</a> 
+    </h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat, at debitis molestias, minima quis laborum delectus quisquam id similique iste sit quos libero recusandae repellat ut illo. Velit, nisi! Obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste minus ad reiciendis unde, consequuntur id harum eveniet sapiente dolor quam eligendi nesciunt eos est earum explicabo, aspernatur esse quis enim!</p>
+   </article>
+
+
+   
+   <article>
+      <h1>
+         <a href="/posts/my-third-post">My Third Post</a> 
+      </h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio ad quae ex laudantium, facilis soluta, repellendus quis animi qui perspiciatis reiciendis illo quod minus possimus modi minima iste repudiandae sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste minus ad reiciendis unde, consequuntur id harum eveniet sapiente dolor quam eligendi nesciunt eos est earum explicabo, aspernatur esse quis enim!</p>
+     </article>
+  </body>
+ </html>
